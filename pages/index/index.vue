ccfd14a7fbaa9b99e5711274ee8ec7c3eccc8839
赵倩倩 <1192117761@qq.com>
2023-04-28 16:47:42 +0800

    fix: type-writer 修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: 赵倩倩 1192117761@qq.com
+ * @Date: 2023-04-28 08:45:34
+ * @LastEditors: 赵倩倩 1192117761@qq.com
+ * @LastEditTime: 2023-04-28 16:41:55
+ * @FilePath: \chatGPT\pages\index\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
 	<view class="container">
 		<view class="type-writer-content" v-for="(item,index) in list" :key="index">
